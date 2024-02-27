0913224c25b3bdc9ad229a97b54a29ad249d4329
Ali Fraz <alifrazkhan92@gmail.com>
2024-02-27 14:31:12 +0000

    Login and LogOut

--- my_site/authenticate/views.py
+++ my_site/authenticate/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
 
-def login(request):
-    return render(request, 'authenticate/login.html', {})
+def login_user(request):
+    if request.method == 'POST':    
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('Welcome to my world !!! '))
+            return redirect('home')
+        else:
+            messages.success(request, ('Error Logging In - Please Try Again...'))
+            return redirect('login')
+    else:   
+        return render(request, 'authenticate/login.html', {})
+
+
+def logout_user(request):
+     logout(request)
+     messages.info(request, 'You are logged out.')
+     return redirect ('home')
 
